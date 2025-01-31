1e2cbfbb7d44e94b86f3d9589a2599a3b56e3530
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2025-01-31 11:19:18 +0000

    remove nix package installation from install script

--- install.sh
+++ install.sh
@@ -3,28 +3,6 @@
 # install nix (single user)
 sh <(curl -L https://nixos.org/nix/install)
 
-# source nix (avoid need to reopon term)
-. ~/.nix-profile/etc/profile.d/nix.sh
-
-# install packages from nix
-nix-env -iA \
-  nixpkgs.antibody \
-  nixpkgs.bat \
-  nixpkgs.direnv \
-  nixpkgs.fzf \
-  nixpkgs.git \
-  nixpkgs.gh \
-  nixpkgs.lazygit \
-  nixpkgs.neovim \
-  nixpkgs.ripgrep \
-  nixpkgs.starship \
-  nixpkgs.stow \
-  nixpkgs.tree \
-  nixpkgs.tmux \
-  nixpkgs.xsel \
-  nixpkgs.yarn \
-  nixpkgs.zsh
-
 # stow all directories
 # stow */ --target=${HOME}
 
