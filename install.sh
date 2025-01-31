3d765aeb14dd20f6d7af59877f53544afe18a956
John Dennehy <8093939+John-Dennehy@users.noreply.github.com>
2025-01-31 11:33:35 +0000

    Remove nix installation and update nodejs and pnpm installation steps in install script

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# install nix (single user)
-sh <(curl -L https://nixos.org/nix/install)
-
 # stow all directories
 # stow */ --target=${HOME}
 
 # OR stow individual directories
 stow fonts --target=${HOME}
 stow git --target=${HOME}
+stow github --target=${HOME}
 stow neovim --target=${HOME}
 stow starship --target=${HOME}
 stow zsh --target=${HOME}
@@ -21,9 +19,12 @@ command -v zsh | sudo tee -a /etc/shells
 # set default shell
 sudo chsh -s $(which zsh) $USER
 
+# TODO: replace antibody?
 # bundle zsh plugins
 antibody bundle <~/.zsh_plugins.txt >~/.zsh_plugins.sh
 
+# TODO: install nvm before running it!
+
 # install nodejs and pnpm
-nvm install --lts
-corepack enable pnpm
+# nvm install --lts
+# corepack enable pnpm
